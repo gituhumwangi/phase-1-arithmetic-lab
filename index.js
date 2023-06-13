406e628f81d32bbd472011a4ea5fd4166b5872f5
dennis <dennis.mwangi@student.moringaschool.com>
2023-06-13 14:45:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code here 
+    let num1 =31
+    let num2 = 2
+    let multiply = num1 * num2
+
+let random = (Math.ceil(Math.random() * 10) + 1)
+
+let num3 = 17
+let num4 = 13
+let mod = num3 % num4
+
+let max =(Math.max(15, 10, 12, 14, 17, 20, 7, 9))
